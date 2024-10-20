0b0ee84dde2c4e2f147b205403f70a963dc90908
Charles Matthews <charles0matthews@gmail.com>
2024-10-20 12:13:05 -0400

    start sketching slew node

---
+++ Source/MetasoundBranches/Private/MetasoundSlewNode.cpp
@@ -0,0 +1,7 @@
+// Name: MetasoundSlewNode
+// Inlets: signal, rise time, fall time (audio, float, float)
+// Outlets: signal (audio)
+
+// Work with seconds/milliseconds instead of samples
+// If so, need to get sample rate
+// Set defaults for rise and fall times (0)
