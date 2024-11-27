2c070a9b76c1d5dd6c2cad7ec7e714b43ea1ef03
Charles Matthews <charles0matthews@gmail.com>
2024-11-27 14:06:04 -0500

    Switch to float and trigger
    I think this works better for now, can revisit the audio version down the line.

--- Source/MetasoundBranches/Private/MetasoundShiftRegisterNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundShiftRegisterNode.cpp
@@ -1,10 +1,10 @@
 #include "MetasoundBranches/Public/MetasoundShiftRegisterNode.h"
-#include "MetasoundExecutableOperator.h"
-#include "MetasoundPrimitives.h"
-#include "MetasoundNodeRegistrationMacro.h"
-#include "MetasoundStandardNodesNames.h"
-#include "MetasoundFacade.h"
-#include "MetasoundParamHelper.h"
+#include "MetasoundExecutableOperator.h"     // TExecutableOperator class
+#include "MetasoundPrimitives.h"             // ReadRef and WriteRef descriptions for bool, int32, float, and string
+#include "MetasoundNodeRegistrationMacro.h"  // METASOUND_LOCTEXT and METASOUND_REGISTER_NODE macros
+#include "MetasoundStandardNodesNames.h"     // StandardNodes namespace
+#include "MetasoundFacade.h"                 // FNodeFacade class, eliminates the need for a fair amount of boilerplate code
+#include "MetasoundParamHelper.h"            // METASOUND_PARAM and METASOUND_GET_PARAM family of macros
 
 #define LOCTEXT_NAMESPACE "MetasoundStandardNodes_ShiftRegisterNode"
 
@@ -12,17 +12,17 @@ namespace Metasound
 {
     namespace ShiftRegisterNodeNames
     {
-        METASOUND_PARAM(InputSignal, "Signal", "Input signal.");
-        METASOUND_PARAM(InputTrigger, "Trigger", "Trigger signal.");
+        METASOUND_PARAM(InputSignal, "Signal", "Input float to the shift register.");
+        METASOUND_PARAM(InputTrigger, "Trigger", "Trigger.");
 
-        METASOUND_PARAM(OutputSignal1, "Shifted Signal 1", "Shifted output signal at stage 1.");
-        METASOUND_PARAM(OutputSignal2, "Shifted Signal 2", "Shifted output signal at stage 2.");
-        METASOUND_PARAM(OutputSignal3, "Shifted Signal 3", "Shifted output signal at stage 3.");
-        METASOUND_PARAM(OutputSignal4, "Shifted Signal 4", "Shifted output signal at stage 4.");
-        METASOUND_PARAM(OutputSignal5, "Shifted Signal 5", "Shifted output signal at stage 5.");
-        METASOUND_PARAM(OutputSignal6, "Shifted Signal 6", "Shifted output signal at stage 6.");
-        METASOUND_PARAM(OutputSignal7, "Shifted Signal 7", "Shifted output signal at stage 7.");
-        METASOUND_PARAM(OutputSignal8, "Shifted Signal 8", "Shifted output signal at stage 8.");
+        METASOUND_PARAM(OutputSignal1, "Stage 1", "Shifted output at stage 1.");
+        METASOUND_PARAM(OutputSignal2, "Stage 2", "Shifted output at stage 2.");
+        METASOUND_PARAM(OutputSignal3, "Stage 3", "Shifted output at stage 3.");
+        METASOUND_PARAM(OutputSignal4, "Stage 4", "Shifted output at stage 4.");
+        METASOUND_PARAM(OutputSignal5, "Stage 5", "Shifted output at stage 5.");
+        METASOUND_PARAM(OutputSignal6, "Stage 6", "Shifted output at stage 6.");
+        METASOUND_PARAM(OutputSignal7, "Stage 7", "Shifted output at stage 7.");
+        METASOUND_PARAM(OutputSignal8, "Stage 8", "Shifted output at stage 8.");
     }
 
     class FShiftRegisterOperator : public TExecutableOperator<FShiftRegisterOperator>
@@ -30,18 +30,18 @@ namespace Metasound
     public:
         FShiftRegisterOperator(
             const FOperatorSettings& InSettings,
-            const FAudioBufferReadRef& InInputSignal,
-            const FAudioBufferReadRef& InInputTrigger)
+            const FFloatReadRef& InInputSignal,
+            const FTriggerReadRef& InInputTrigger)
             : InputSignal(InInputSignal)
             , InputTrigger(InInputTrigger)
-            , OutputSignal1(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal2(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal3(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal4(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal5(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal6(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal7(FAudioBufferWriteRef::CreateNew(InSettings))
-            , OutputSignal8(FAudioBufferWriteRef::CreateNew(InSettings))
+            , OutputSignal1(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal2(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal3(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal4(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal5(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal6(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal7(FFloatWriteRef::CreateNew(0.0f))
+            , OutputSignal8(FFloatWriteRef::CreateNew(0.0f))
             , ShiftedValue1(0.0f)
             , ShiftedValue2(0.0f)
             , ShiftedValue3(0.0f)
@@ -50,7 +50,6 @@ namespace Metasound
             , ShiftedValue6(0.0f)
             , ShiftedValue7(0.0f)
             , ShiftedValue8(0.0f)
-            , PreviousTriggerValue(0.0f)
         {
         }
 
@@ -60,18 +59,18 @@ namespace Metasound
 
             static const FVertexInterface Interface(
                 FInputVertexInterface(
-                    TInputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(InputSignal)),
-                    TInputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(InputTrigger))
+                    TInputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(InputSignal)),
+                    TInputDataVertexModel<FTrigger>(METASOUND_GET_PARAM_NAME_AND_METADATA(InputTrigger))
                 ),
                 FOutputVertexInterface(
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal1)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal2)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal3)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal4)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal5)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal6)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal7)),
-                    TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal8))
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal1)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal2)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal3)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal4)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal5)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal6)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal7)),
+                    TOutputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutputSignal8))
                 )
             );
 
@@ -86,14 +85,14 @@ namespace Metasound
 
                     FNodeClassMetadata Metadata;
 
-                    Metadata.ClassName = { StandardNodes::Namespace, TEXT("Shift Register"), StandardNodes::AudioVariant };
+                    Metadata.ClassName = { StandardNodes::Namespace, TEXT("Shift Register"), TEXT("Float") };
                     Metadata.MajorVersion = 1;
                     Metadata.MinorVersion = 0;
                     Metadata.DisplayName = METASOUND_LOCTEXT("ShiftRegisterNodeDisplayName", "Shift Register");
                     Metadata.Description = METASOUND_LOCTEXT("ShiftRegisterNodeDesc", "Shift register node with eight stages.");
                     Metadata.Author = PluginAuthor;
                     Metadata.PromptIfMissing = PluginNodeMissingPrompt;
-                    Metadata.DefaultInterface = DeclareVertexInterface();
+                    Metadata.DefaultInterface = NodeInterface;
                     Metadata.CategoryHierarchy = { METASOUND_LOCTEXT("Custom", "Branches") };
                     Metadata.Keywords = TArray<FText>();
 
@@ -141,8 +140,8 @@ namespace Metasound
             const Metasound::FDataReferenceCollection& InputCollection = InParams.InputDataReferences;
             const Metasound::FInputVertexInterface& InputInterface = DeclareVertexInterface().GetInputInterface();
 
-            TDataReadReference<FAudioBuffer> InputSignal = InputCollection.GetDataReadReferenceOrConstructWithVertexDefault<FAudioBuffer>(InputInterface, METASOUND_GET_PARAM_NAME(InputSignal), InParams.OperatorSettings);
-            TDataReadReference<FAudioBuffer> InputTrigger = InputCollection.GetDataReadReferenceOrConstructWithVertexDefault<FAudioBuffer>(InputInterface, METASOUND_GET_PARAM_NAME(InputTrigger), InParams.OperatorSettings);
+            TDataReadReference<float> InputSignal = InputCollection.GetDataReadReferenceOrConstructWithVertexDefault<float>(InputInterface, METASOUND_GET_PARAM_NAME(InputSignal), InParams.OperatorSettings);
+            TDataReadReference<FTrigger> InputTrigger = InputCollection.GetDataReadReferenceOrConstruct<FTrigger>(METASOUND_GET_PARAM_NAME(InputTrigger), InParams.OperatorSettings);
 
             return MakeUnique<FShiftRegisterOperator>(
                 InParams.OperatorSettings,
@@ -153,24 +152,15 @@ namespace Metasound
 
         void Execute()
         {
-            int32 NumFrames = InputSignal->Num();
-            const float* SignalData = InputSignal->GetData();
-            const float* TriggerData = InputTrigger->GetData();
+            InputTrigger->ExecuteBlock(
+                // Pre-trigger lambda (called before any triggers in the block)
+                [](int32 StartFrame, int32 EndFrame)
+                {
+                    // No action needed before triggers
+                },
 
-            float* OutputData1 = OutputSignal1->GetData();
-            float* OutputData2 = OutputSignal2->GetData();
-            float* OutputData3 = OutputSignal3->GetData();
-            float* OutputData4 = OutputSignal4->GetData();
-            float* OutputData5 = OutputSignal5->GetData();
-            float* OutputData6 = OutputSignal6->GetData();
-            float* OutputData7 = OutputSignal7->GetData();
-            float* OutputData8 = OutputSignal8->GetData();
-
-            for (int32 i = 0; i < NumFrames; ++i)
-            {
-                float CurrentTriggerValue = TriggerData[i];
-
-                if (PreviousTriggerValue <= 0.0f && CurrentTriggerValue > 0.0f)
+                // On-trigger lambda (called for each trigger event)
+                [&](int32 StartFrame, int32 EndFrame)
                 {
                     ShiftedValue8 = ShiftedValue7;
                     ShiftedValue7 = ShiftedValue6;
@@ -179,34 +169,33 @@ namespace Metasound
                     ShiftedValue4 = ShiftedValue3;
                     ShiftedValue3 = ShiftedValue2;
                     ShiftedValue2 = ShiftedValue1;
-                    ShiftedValue1 = SignalData[i];
+                    ShiftedValue1 = *InputSignal;
                 }
+            );
 
-                OutputData1[i] = ShiftedValue1;
-                OutputData2[i] = ShiftedValue2;
-                OutputData3[i] = ShiftedValue3;
-                OutputData4[i] = ShiftedValue4;
-                OutputData5[i] = ShiftedValue5;
-                OutputData6[i] = ShiftedValue6;
-                OutputData7[i] = ShiftedValue7;
-                OutputData8[i] = ShiftedValue8;
-
-                PreviousTriggerValue = CurrentTriggerValue;
-            }
+            // Update outputs
+            *OutputSignal1 = ShiftedValue1;
+            *OutputSignal2 = ShiftedValue2;
+            *OutputSignal3 = ShiftedValue3;
+            *OutputSignal4 = ShiftedValue4;
+            *OutputSignal5 = ShiftedValue5;
+            *OutputSignal6 = ShiftedValue6;
+            *OutputSignal7 = ShiftedValue7;
+            *OutputSignal8 = ShiftedValue8;
         }
 
     private:
-        FAudioBufferReadRef InputSignal;
-        FAudioBufferReadRef InputTrigger;
+        FFloatReadRef InputSignal;
+        FTriggerReadRef InputTrigger;
 
-        FAudioBufferWriteRef OutputSignal1;
-        FAudioBufferWriteRef OutputSignal2;
-        FAudioBufferWriteRef OutputSignal3;
-        FAudioBufferWriteRef OutputSignal4;
-        FAudioBufferWriteRef OutputSignal5;
-        FAudioBufferWriteRef OutputSignal6;
-        FAudioBufferWriteRef OutputSignal7;
-        FAudioBufferWriteRef OutputSignal8;
+        FFloatWriteRef OutputSignal1;
+        FFloatWriteRef OutputSignal2;
+        FFloatWriteRef OutputSignal3;
+        FFloatWriteRef OutputSignal4;
+        FFloatWriteRef OutputSignal5;
+        FFloatWriteRef OutputSignal6;
+        FFloatWriteRef OutputSignal7;
+        FFloatWriteRef OutputSignal8;
 
         float ShiftedValue1;
         float ShiftedValue2;
@@ -216,7 +205,6 @@ namespace Metasound
         float ShiftedValue6;
         float ShiftedValue7;
         float ShiftedValue8;
-        float PreviousTriggerValue;
     };
 
     class FShiftRegisterNode : public FNodeFacade
