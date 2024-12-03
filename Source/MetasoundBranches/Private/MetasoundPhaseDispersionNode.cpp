2743fb6cda14b13842c8b553bda4945f9ef21104
Charles Matthews <charles0matthews@gmail.com>
2024-12-03 17:09:11 -0500

    fix author name

--- Source/MetasoundBranches/Private/MetasoundPhaseDispersionNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundPhaseDispersionNode.cpp
@@ -58,7 +58,7 @@ namespace Metasound
                 Metadata.MinorVersion = 0;
                 Metadata.DisplayName = METASOUND_LOCTEXT("PhaseDispersionNodeDisplayName", "Phase Dispersion");
                 Metadata.Description = METASOUND_LOCTEXT("PhaseDispersionNodeDesc", "Applies ten allpass filters to the incoming signal.");
-                Metadata.Author = PluginAuthor;
+                Metadata.Author = "Charles Matthews";
                 Metadata.PromptIfMissing = PluginNodeMissingPrompt;
                 Metadata.DefaultInterface = DeclareVertexInterface();
                 Metadata.CategoryHierarchy = { METASOUND_LOCTEXT("Custom", "Branches") };
