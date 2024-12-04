94e9e05e38c04a00a1a0a38058192fe06f234ba3
Charles Matthews <charles0matthews@gmail.com>
2024-12-04 15:57:31 -0500

    Update MetasoundClickNode.cpp
    Remove unnecessary inclusions from the original dust node.

--- Source/MetasoundBranches/Private/MetasoundClickNode.cpp
+++ Source/MetasoundBranches/Private/MetasoundClickNode.cpp
@@ -5,8 +5,6 @@
 #include "MetasoundStandardNodesNames.h"     // StandardNodes namespace
 #include "MetasoundFacade.h"                 // FNodeFacade class, eliminates the need for a fair amount of boilerplate code
 #include "MetasoundParamHelper.h"            // METASOUND_PARAM and METASOUND_GET_PARAM family of macros
-#include "Math/UnrealMathUtility.h"          // For FMath functions
-#include "Misc/DateTime.h"                   // For FDateTime::UtcNow()
 
 // Required for ensuring the node is supported by all languages in engine. Must be unique per MetaSound.
 #define LOCTEXT_NAMESPACE "MetasoundStandardNodes_ClickNode"
