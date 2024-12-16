1b716a522d010643a7e4ffd87be6979d17b48387
Charles Matthews <charles0matthews@gmail.com>
2024-12-16 09:25:29 -0500

    Update MetasoundBranches.Build.cs
    Remove unnecessary paths

--- Source/MetasoundBranches.Build.cs
+++ Source/MetasoundBranches.Build.cs
@@ -5,19 +5,7 @@ public class MetasoundBranches : ModuleRules
     public MetasoundBranches(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-        
-        PublicIncludePaths.AddRange(
-            new string[] {
-                "MetasoundBranches/Public"
-            }
-        );
-
-        PrivateIncludePaths.AddRange(
-            new string[] {
-                "MetasoundBranches/Private",
-            }
-        );
-
+       
         PublicDependencyModuleNames.AddRange(
             new string[]
             {
