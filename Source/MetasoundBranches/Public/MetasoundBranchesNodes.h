98351da4afbca95b185cb5d59bd620c704678cac
Charles Matthews <charles0matthews@gmail.com>
2025-02-06 20:40:54 -0500

    fix error from fab build

---
+++ Source/MetasoundBranches/Public/MetasoundBranchesNodes.h
@@ -0,0 +1,13 @@
+// Copyright 2025 Charles Matthews. All Rights Reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+
+namespace Metasound
+{
+    namespace StandardNodes
+    {
+        const FName Namespace = "UE";
+        const FName AudioVariant = "Audio";
+    }
+}
